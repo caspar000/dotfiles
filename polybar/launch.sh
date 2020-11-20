7d1514ab5b99d87824634dbe514d9437bee03c42
caspar000 <nikolaev2000@protonmail.com>
2020-11-20 20:06:50 +0400

    ARCH-Melchior config 20-11-2020

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,7 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar vanir -r &
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload vanir &
+        MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload bottom &
+    done
+else
+    # Launch Polybar, using default config location ~/.config/polybar/config
+    polybar -c ~/.config/polybar/config.ini vanir -r &
+fi
 
 echo "Polybar launched..."
