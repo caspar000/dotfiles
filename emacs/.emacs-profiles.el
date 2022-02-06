61e90f403d3d64efe5827a24410526915c7fa1bf
caspar000 <nikolaev2000@protonmail.com>
2022-02-06 21:16:33 +0400

    emacs configuration with chemacs2

---
+++ emacs/.emacs-profiles.el
@@ -0,0 +1,3 @@
+(("default" . ((user-emacs-directory . "~/.local/emconfigs/.doomemacs")
+               (env . (("DOOMDIR"    . "~/.config/.doom.d")))))
+ ("efs"     . ((user-emacs-directory . "~/.local/emconfigs/.efsemacs"))))
